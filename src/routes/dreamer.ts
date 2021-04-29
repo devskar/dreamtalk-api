45be818becd06af4f28224872faa29c5c15e9316
oskar <ooskar@gmx.de>
2021-04-29 16:55:53 +0200

    rewriting at the moment

--- src/routes/dreamer.ts
+++ src/routes/dreamer.ts
@@ -1,49 +1,13 @@
-import { Dreamer } from './../entity/Dreamer';
 import express from 'express';
-import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-  const { username, password, email } = req.body;
+// CREATING A DREAMER
+router.post('/', (req, res, next) => {});
 
-  console.log(username, password, email);
+router.get('/', (req, res, next) => {});
 
-  if (password) {
-    bcrypt.hash(password, 15).then((hashedPassword) => {
-      const temp = Dreamer.create({
-        email,
-        username,
-        password: hashedPassword,
-      });
-
-      temp.save().then(() => {
-        res.status(200).json({
-          message: 'Successfully created user!',
-          id: temp.id,
-        });
-      });
-    });
-  }
-});
-
-// get all dreamer
-router.get('/', (req, res, next) => {
-  Dreamer.find().then((dreamer) => {
-    res.json(dreamer);
-  });
-});
-
-// get a dreamer by id
-router.get('/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  console.log(id);
-
-  Dreamer.findOne(id).then((dream) => {
-    console.log(dream);
-  });
-});
+router.get('/:id', (req, res, next) => {});
 
 router.put('/:id', (req, res, next) => {});
 
